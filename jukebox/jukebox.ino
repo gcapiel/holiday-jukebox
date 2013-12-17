0818c2eb27c74c611dd15346cc836cf11bf317e2
Gerardo Capiel <gcapiel@alum.mit.edu>
2013-12-16 22:59:24 -0800

    multiline support

--- jukebox/jukebox.ino
+++ jukebox/jukebox.ino
@@ -10,15 +10,14 @@ unsigned long lastPress = 0;
 const unsigned long timeOutDisplay = 25000;
 const unsigned long timeOutButton = 2500;
 int msgOneState = 1;
-char* songFiles[] = {"O_Tannenbaum","Santa_Baby","White_Christmas","O_Tannenbaum","O_Tannenbaum"};
-char* songTitles[] = {"O Tannenbaum","Santa Baby","White Christmas","O Tannenbaum","O Tannenbaum"};
+char* songFiles[] = {"O_Tannenbaum","Santa_Baby","White_Christmas","Baby_Its_Cold_Outside","Joy_to_the_World"};
+char* songTitles[] = {"O Tannenbaum","Santa Baby","White Christmas","Baby, It's|Cold Outside","Joy to the World"};
 int currentSong;
 
 TKLCD_Local lcd = TKLCD_Local();
 TKButton button(A0); 
 
 void setup() {
-  //setTime(15,0,0,21,9,2013);
   lcd.begin();
   Serial1.begin(9600);
   Serial1.println("msg:Jukebox ready"); 
@@ -36,9 +35,17 @@ void loop() {
       lcd.display();
       lcd.setBrightness(255);
       lcd.setCursor(0,0);
-      lcd.print(songTitles[currentSong]);
-      lcd.setCursor(0,1);
-      lcd.print("");
+      String currentSongTitle = songTitles[currentSong];
+      if (currentSongTitle.indexOf('|') > 0) {
+        lcd.setCursor(0,0);
+        lcd.print(currentSongTitle.substring(0,currentSongTitle.indexOf('|')));
+        lcd.setCursor(0,1);
+        lcd.print(currentSongTitle.substring(currentSongTitle.indexOf('|')+1));
+      }
+      else {
+        lcd.setCursor(0,0);
+        lcd.print(songTitles[currentSong]);
+      }
     }
   } else {
     lcd.noDisplay();
